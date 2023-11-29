438d31f276841577e54f2f8d44cb5a76b9bc4b14
Nikita <dolganov.alfa@gmail.com>
2023-11-29 13:18:38 +0300

    добавил

--- 001hello_world/Program.cs
+++ 001hello_world/Program.cs
@@ -1,3 +1,6 @@
 ﻿Console.WriteLine("2 Hello, World!");
 Console.WriteLine("dotnet new console - создать новую консоль");
 Console.WriteLine("dotnet run - запустить компеляцию");
+Console.WriteLine("Write - Ввод в одну строку");
+Console.WriteLine("WriteLine - В конце перейти на новую строку");
+Console.WriteLine("ReadLine - Считать строку из терминала");
