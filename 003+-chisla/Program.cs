d20892193cad01763153906bd20af779ad43b2ce
Nikita <dolganov.alfa@gmail.com>
2023-11-29 13:59:19 +0300

    add Random

--- 003+-chisla/Program.cs
+++ 003+-chisla/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 83;
-int numberB = 17;
+﻿int numberA = new Random().Next(1,100); // 1 2 3 4 5 ... 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100);
+Console.WriteLine(numberB);
 int rezultat = numberA + numberB;
 Console.WriteLine(rezultat);
