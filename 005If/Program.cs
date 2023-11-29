54f12f5d702ace9159f918aadc699ab61669c1c1
Nikita <dolganov.alfa@gmail.com>
2023-11-29 14:19:42 +0300

    if

---
+++ 005If/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nikita")
+{
+    Console.Write("Увуф вы вернулись госпадин ");
+    Console.Write(username);
+}
+else
+{
+    Console.Write("Ну привет ");
+    Console.Write(username);
+}
